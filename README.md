d760063e5b416be02b63659b310561fdec8b0e94
Anusha Upadhyay <114226810+anushaisawesome@users.noreply.github.com>
2026-02-04 14:57:53 +0000

    Add files via upload
    also has data.yaml which is specifically a ball dataset for future use. this demo isn't trained on it though, it's the pretrained YOLO26

---
+++ README.md
@@ -0,0 +1,9 @@
+<b> run these in a new terminal (mac)</b>
+<br><br>
+python3.12 -m venv yolo_env
+source yolo_env/bin/activate
+pip install --upgrade pip
+pip install ultralytics lap
+python ~/Desktop/unibots-yolo-demo1/yolo-demo.py (or whatever the filepath is for u)
+<br><br><br>
+<b>get rid of webcam window by trashing the terminal i havent figured that one out yet 😭😭😭 </b>
---
+++ yolo-demo.py
@@ -0,0 +1,35 @@
+from ultralytics import YOLO
+import cv2
+
+# start webcam
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+model = YOLO("yolo26n.pt")  # load YOLO26 model
+
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        # Run YOLO26 tracking on the frame, persisting tracks between frames
+        results = model.track(frame, persist=True)
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        cv2.imshow("YOLO26 Tracking", annotated_frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
